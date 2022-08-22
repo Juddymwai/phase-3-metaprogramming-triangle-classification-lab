75bac8a1d583ed3dcf00b8614e24514e60d602e7
Judy Mwai <juddiemukami@gmail.com>
2022-08-22 21:10:53 +0300

    commit tree d2535adf59b80b9d4932ac739a79cdc0f92c5c73

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize (a, b, c)
+    @a= a
+    @b=b
+    @c=c
+  end
+  def kind
+
+    if a+b>c && a+c>b && b+c>a 
+      if a == b && a == c && b == c
+        :equilateral
+      elsif a == b || a == c || b == c
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      begin
+        raise TriangleError
+      end
+    end
+
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Not a valid triangle"
+    end
+
+  end
+
 end
+
+
+
